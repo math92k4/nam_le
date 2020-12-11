f5a0ba6b6c3c941f9ab5a68a09371125167f2fc4
Mathias Dahl Jensen <mathias@Mathiass-MacBook-Pro-4.local>
2020-12-11 21:48:34 +0100

    footer, burger menu og start på forside

--- script.js
+++ script.js
@@ -1,19 +1,51 @@
 document.addEventListener("DOMContentLoaded", getHeader);
 
-//hent menu ind på alle sider//
+//henter menu fra header.html ind på sidens header-tag
 
 async function getHeader() {
     let header
+
+    //header.html targetes, og indholdet fetches ind på værdien "let header"
     let headerUrl = "header.html";
     let headerData = await fetch(headerUrl);
     header = await headerData.text();
 
+    //Header-tagget targetes, og indholdet heri bliver = html'en fra "let header"
     document.querySelector("header").innerHTML = header;
-    console.log(getHeader);
+
+    //næste funktioner i rækken kaldes
+    constructMenu();
     getFooter();
+}
+
+
+
+//konstruerer burgermenuen
+function constructMenu() {
+
+    const menuBtn = document.querySelector(".menu_btn");
+    const menuNav = document.querySelector("#menu");
+    let menuOpen = false;
+
+    menuBtn.addEventListener("click", () => {
+        if (menuOpen == false) {
+            menuBtn.classList.add("open");
+            menuNav.classList.remove("hidden");
+            menuOpen = true;
+        } else {
+            menuBtn.classList.remove("open");
+            menuNav.classList.add("hidden");
+            menuOpen = false;
+        }
+    })
 
 
 }
+
+
+
+
+
 
 //hent footer ind på alle sider//
 
