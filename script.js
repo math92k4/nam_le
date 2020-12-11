aed4a6f38ad45621da50f85effdea6b1202bfcff
Rasmus Krogsgaard Møller <Rasmus@Rasmuss-Air-5.lan>
2020-12-11 14:08:29 +0100

    flyttede script under footer and other stories

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+document.addEventListener("DOMContentLoaded", getHeader);
+
+//hent menu ind på alle sider//
+
+async function getHeader() {
+    let header
+    let headerUrl = "header.html";
+    let headerData = await fetch(headerUrl);
+    header = await headerData.text();
+
+    document.querySelector("header").innerHTML = header;
+    console.log(getHeader);
+    getFooter();
+
+
+}
+
 //hent footer ind på alle sider//
 
 async function getFooter() {
@@ -5,7 +22,7 @@ async function getFooter() {
     let footerUrl = "footer.html";
     let footerData = await fetch(footerUrl);
     footer = await footerData.text();
-
     document.querySelector("footer").innerHTML = footer;
+    console.log(getFooter);
 
 }
